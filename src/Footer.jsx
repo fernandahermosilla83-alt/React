d7d49d6460386df01f59b7a839e0dce0d1aecea3
fernandahermosilla83-alt <fernandahermosilla83@gmail.com>
2026-02-16 17:32:05 -0300

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer style={{padding:20,textAlign:"center",opacity:0.7}}>
-      © {new Date().getFullYear()} Guías Fitness
-    </footer>
-  );
-};
-
-export default Footer;
